07b85629c4212246cb976559e663b19fdb07ca65
Herbert Campos <wohoforever@yahoo.com>
2019-08-31 16:32:24 -0700

    added onto apiroutes the match making process

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,6 +8,33 @@ module.exports = (app) => {
     });
 
     app.post('/api/friends',(req, res) => {
-        friendData.push(req.body);
+        let bestMatch ={
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+        console.log(req.body);
+
+        let userData = req.body;
+        let userScores = userData.scores;
+
+        console.log(userScores);
+
+        let totalDifference = 0;
+
+        for (let i = 0; i < friendData.length; i++) {
+            console.log(friendData[i]);
+            totalDifference = 0;
+            for (let j = 0; j < friendData[i].scores[j]; j++) {
+                totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendData[i].scores[j]));
+                if (totalDifference <= bestMatch.friendDifference) {
+                    bestMatch.name = friendData[i].name;
+                    bestMatch.photo = friendData[i].photo;
+                    bestMatch.friendDifference = totalDifference;
+                }
+            }
+        }
+        friendData.push(userData);
+        res.json(bestMatch);
     })
 }
